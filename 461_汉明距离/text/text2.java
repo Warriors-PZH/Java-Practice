3d73451fd1f13bebca8d3ceaf27ee8f2bf4966b1
Warriors-PZH <82789482+Warriors-PZH@users.noreply.github.com>
2021-05-30 10:54:16 +0800

    Create text2.java

---
+++ 461_汉明距离/text/text2.java
@@ -0,0 +1,15 @@
+public class text2 {
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 4;
+		int result = x ^ y;
+		byte[] bytes = Integer.toBinaryString(result).getBytes();
+		result = 0;
+		
+		for(int i = 0;i < bytes.length;i++) {
+			if((char)bytes[i] == '1') {
+				result++;
+			}
+		}
+	}
+}
